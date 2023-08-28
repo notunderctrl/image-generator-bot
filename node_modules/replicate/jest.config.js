a6b3ffa5f322a462251e570530200d7d6a375445
Jay Robinson <139034125+jaywrobinson@users.noreply.github.com>
2023-08-28 19:44:39 -0400

    ok

---
+++ node_modules/replicate/jest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line jsdoc/valid-types
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json'
+    }],
+  },
+};
